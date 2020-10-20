d71d9213c899c9702ab85551e52c05410045ed25
CMD <gautamkailashsingh@gmail.com>
2020-10-20 23:23:48 +0530

    [Gautam] Refactored the code to use generic method.

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -7,15 +7,15 @@ namespace FindMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Integer Checking. Find max Number. Numbers are: 11, 22 and 33.");
-            int outputInteger = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            int outputInteger = Generic.MaximumNumber<int>(11, 22, 33);
             Console.WriteLine(outputInteger);
 
             Console.WriteLine("Float Number Checking. Find max Number. Numbers are: 11.1, 22.2 and 33.3.");
-            float outputFloat = MaximumNumberCheck.MaximumFloatNumber(11.1f, 22.2f, 33.3f);
+            float outputFloat = Generic.MaximumNumber<float>(11.1f, 22.2f, 33.3f);
             Console.WriteLine(outputFloat);
 
             Console.WriteLine("Float Number Checking. Find max Number. Numbers are: \"11\", \"22\" and \"33\".");
-            string outputString=MaximumNumberCheck.MaximumStringNumber("11", "22", "33");
+            string outputString=Generic.MaximumNumber<string>("11", "22", "33");
             Console.WriteLine(outputString);
         }
     }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,7 @@
 using FindMaximumNumber;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace UnitTestProject1
 {
@@ -33,6 +35,36 @@ namespace UnitTestProject1
         public void MaximumStringNumber_PassThreeNumbers_ReturnMaximumNumber(string a, string b, string c, string max)
         {
             string result = MaximumNumberCheck.MaximumStringNumber(a, b, c);
+            Assert.AreEqual(result, max);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 3)]
+        [DataRow(1, 4, 2, 4)]
+        [DataRow(5, 2, 1, 5)]
+        public void MaximumNumberGenericInteger_PassThreeNumbers_ReturnMaximumNumber(int a, int b, int c, int max)
+        {
+            int result = MaximumNumberCheck.MaximumIntegerNumber(a, b, c);
+            Assert.AreEqual(result, max);
+        }
+
+        [TestMethod]
+        [DataRow(1.1f, 2.2f, 3.3f, 3.3f)]
+        [DataRow(1.1f, 4.4f, 2.2f, 4.4f)]
+        [DataRow(5.5f, 2.2f, 1.1f, 5.5f)]
+        public void MaximumNumberGenericFloat_PassThreeNumbers_ReturnMaximumNumber(float a, float b, float c, float max)
+        {
+            float result = Generic.MaximumNumber<float>(a, b, c);
+            Assert.AreEqual(result, max);
+        }
+
+        [TestMethod]
+        [DataRow("1", "2", "3", "3")]
+        [DataRow("1", "4", "2", "4")]
+        [DataRow("5", "2", "1", "5")]
+        public void MaximumNumberGenericString_PassThreeNumbers_ReturnMaximumNumber(string a, string b, string c, string max)
+        {
+            string result = Generic.MaximumNumber<string>(a, b, c);
             Assert.AreEqual(result, max);
         }
     }
