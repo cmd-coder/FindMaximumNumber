fa452a2b0d915fece04abc113aa5edf2ca133b05
CMD <gautamkailashsingh@gmail.com>
2020-10-20 23:48:50 +0530

    [Gautam] Changed the code to print the output from the generic class.

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -9,9 +9,11 @@ namespace FindMaximumNumber
             int[] intArray = { 112, 344, 432, 555, 678 };
             Generic<int> generic = new Generic<int>(intArray);
             generic.PrintMaxValue();
+
             double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
             Generic<double> genericDouble = new Generic<double>(doubleArray);
             genericDouble.PrintMaxValue();
+
             string[] stringArray = { "111", "222", "333", "55", "999" };
             Generic<string> genericString = new Generic<string>(stringArray);
             genericString.PrintMaxValue();
